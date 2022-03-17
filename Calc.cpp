82275b54c19afda785d6410283719a2b4cabc115
Muhammad Riyas Sk <101698135+MuhammadRiyassk@users.noreply.github.com>
2022-03-17 09:36:27 +0530

    Update Calc.cpp

--- Calc.cpp
+++ Calc.cpp
@@ -7,25 +7,19 @@ float div;
 cout<<"Enter your choice \n";
 cout<<"1.Adition \n2.Subtraction  \n3.Multiplication  \n4.Divission\n";
 cin>>ch;
-switch(ch){
-case 1:cout<<"Enter the two numbers:";
+cout<<"Enter the two numbers:";
 cin>>num1>>num2;
-sum=num1+num2;
+switch(ch){
+case 1:sum=num1+num2;
 cout<<"Adition is :"<<sum;
 break;
-case 2:cout<<"Enter the two numbers:";
-cin>>num1>>num2;
-sub=num1-num2;
+case 2:sub=num1-num2;
 cout<<"Subtraction is:"<<sub;
 break;
-case 3:cout<<"Enter the two numbers:";
-cin>>num1>>num2;
-mul=num1*num2;
+case 3:mul=num1*num2;
 cout<<"Multiplication is:"<<mul;
 break;
-case 4:cout<<"Enter the two numbers:";
-cin>>num1>>num2;
-div=num1/num2;
+case 4:div=num1/num2;
 cout<<"Divission is:"<<div;
 }
 
